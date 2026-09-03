396dabd50c3c4e7c3f855ab1eb56247a51f0a8b9
noefazun-sketch <noefazun@gmail.com>
2026-09-03 23:54:50 +0000

    Finish Lab 2 intro

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
  
   std::cout << "Name: Noe Zuniga\n";
   std::cout << "Interest: I like watching football\n";
-  std::cout << "CIS 5 goal: I would ike to run programs efficiently. \n";
+  std::cout << "CIS 5 goal: I would like to run programs efficiently. \n";
   std::cout << "Tool path: GitHub Codespaces\n";
   std::cout << "Fun fact: I can bench press 265 pounds\n";
   return 0;
